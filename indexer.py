14e9ff49bb3f3265bab0617416c581b22f5639ae
Birgit Bachler <hypertext@birgitbachler.com>
2015-09-05 18:40:57 +1200

    added URL

--- indexer.py
+++ indexer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+################################################################################
+#   iRGBit's indexhelper for reading texts and helping with creating an index  #
+#   http://www.github.com/iRGBit                                               #
+################################################################################
 
 yourFile = 'files/sample.txt'
 yourStopWords = 'stop-words_english_1_en.txt'
@@ -11,25 +15,29 @@ bookWords = [el.rstrip(string.punctuation).lstrip(string.punctuation) for el in 
 
 stopWords = open(yourStopWords).read().decode("utf-8-sig").encode("utf-8").splitlines()
 
+# remove stopWords from finalWords
 finalWords = [x for x in bookWords if x not in stopWords]
 
+# count single occurences of words
 from collections import Counter
 topWords = Counter(finalWords)
 
+# calculate average frequency of words to compute average frequency in your text
 total = 0
-
 for w in topWords:
     total += topWords[w]
-
 frequent = total/(len(topWords))
 
-tops = {k:v for (k,v) in topWords.iteritems() if v > frequent}
+# only add words that have more than average frequency
+tops = {k:v for (k,v) in topWords.iteritems() if v >= frequent}
 
 # sort by word count
-#final = sorted(tops.items(), key=lambda x: x[1], reverse=True)
+#cfinal = sorted(tops.items(), key=lambda x: x[1], reverse=True)
 
 #sort Alphabetically
 final = sorted(tops.items(), key=lambda x: x[0])
 
 for x in range(len(final)):
     print '%s: %s' % (final[x][0], final[x][1])
+
+#bye!
