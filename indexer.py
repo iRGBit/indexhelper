5f4b414d21844f1372faa3a22eb3d184ad7862e1
Birgit Bachler <hypertext@birgitbachler.com>
2015-09-05 15:14:53 +1200

    added stopWords

--- indexer.py
+++ indexer.py
@@ -2,9 +2,21 @@
 
 import string
 
-
 punct = set(string.punctuation)
-bookWords = open('files/romeo-and-juliet.txt').read().lower().split()
+bookWords = open('files/huckleberry-finn.txt').read().lower().split()
 bookWords = [el.rstrip(string.punctuation).lstrip(string.punctuation) for el in bookWords]
 
-print bookWords
+#print bookWords
+
+stopWords = open('stop-words_english_1_en.txt').read().decode("utf-8-sig").encode("utf-8").splitlines()
+print stopWords
+
+[x for x in bookWords if x not in stopWords]
+
+#print bookWords
+
+
+from collections import Counter
+topWords = Counter(bookWords)
+
+print topWords
