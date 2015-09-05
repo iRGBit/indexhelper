c69ca3a81e7e444ad39dc102d059abae32102f37
Birgit Bachler <hypertext@birgitbachler.com>
2015-09-05 19:31:06 +1200

    added sys argv input

--- indexer.py
+++ indexer.py
@@ -1,43 +1,68 @@
 #!/usr/bin/python
-################################################################################
-#   iRGBit's indexhelper for reading texts and helping with creating an index  #
-#   http://www.github.com/iRGBit                                               #
-################################################################################
 
-yourFile = 'files/sample.txt'
-yourStopWords = 'stop_words.txt'
 
+import sys
 import string
 
-punct = set(string.punctuation)
-bookWords = open(yourFile).read().decode("unicode-escape").encode("ascii", "ignore").lower().split()
-bookWords = [el.rstrip(string.punctuation).lstrip(string.punctuation) for el in bookWords]
+defaultFile = 'files/sample.txt'
+defaultStopWords = 'stop_words.txt'
 
-stopWords = open(yourStopWords).read().decode("utf-8-sig").encode("utf-8").splitlines()
+def main():
+    if len(sys.argv) > 3:
+        print
+        print "Usage: python indexer.py <yourFile> <stopWords>"
+        print "If no arguments are given %s and %s will be used as default files" % (defaultFile, defaultStopWords)
+        print
+        sys.exit()
+    elif len(sys.argv) == 3:
+        yourStopWords = sys.argv[2]
+        yourFile = sys.argv[1]
 
-# remove stopWords from finalWords
-finalWords = [x for x in bookWords if x not in stopWords]
+    elif len(sys.argv) == 2:
+        yourStopWords = defaultStopWords
+        yourFile = sys.argv[1]
 
-# count single occurences of words
-from collections import Counter
-topWords = Counter(finalWords)
+    elif len(sys.argv) == 1:
+        yourStopWords = defaultStopWords
+        yourFile = defaultFile
 
-# calculate average frequency of words to compute average frequency in your text
-total = 0
-for w in topWords:
-    total += topWords[w]
-frequent = total/(len(topWords))
+    print 'Using %s as file and %s as stop word reference.' % (yourFile, yourStopWords)
+    indexThem(yourFile, yourStopWords)
 
-# only add words that have more than average frequency
-tops = {k:v for (k,v) in topWords.iteritems() if v >= frequent}
+def indexThem(yourFile, yourStopWords):
+    punct = set(string.punctuation)
+    bookWords = open(yourFile).read().decode("unicode-escape").encode("ascii", "ignore").lower().split()
+    bookWords = [el.rstrip(string.punctuation).lstrip(string.punctuation) for el in bookWords]
 
-# sort by word count
-#cfinal = sorted(tops.items(), key=lambda x: x[1], reverse=True)
+    stopWords = open(yourStopWords).read().decode("utf-8-sig").encode("utf-8").splitlines()
 
-#sort Alphabetically
-final = sorted(tops.items(), key=lambda x: x[0])
+    # remove stopWords from finalWords
+    finalWords = [x for x in bookWords if x not in stopWords]
 
-for x in range(len(final)):
-    print '%s: %s' % (final[x][0], final[x][1])
+    # count single occurences of words
+    from collections import Counter
+    topWords = Counter(finalWords)
 
-#bye!
+    # calculate average frequency of words to compute average frequency in your text
+    total = 0
+    for w in topWords:
+        total += topWords[w]
+    frequent = total/(len(topWords))
+
+    # only add words that have more than average frequency
+    tops = {k:v for (k,v) in topWords.iteritems() if v >= frequent}
+
+    # sort by word count
+    #cfinal = sorted(tops.items(), key=lambda x: x[1], reverse=True)
+
+    #sort Alphabetically
+    final = sorted(tops.items(), key=lambda x: x[0])
+
+    for x in range(len(final)):
+        print '%s: %s' % (final[x][0], final[x][1])
+
+    #bye!
+
+
+if __name__ == '__main__':
+  main()
