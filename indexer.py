d78d289ce0ae7abe38e80b1f0485cce01c2c9e8a
Birgit Bachler <hypertext@birgitbachler.com>
2015-09-05 18:10:40 +1200

    final formatting of output

--- indexer.py
+++ indexer.py
@@ -3,7 +3,7 @@
 import string
 
 punct = set(string.punctuation)
-bookWords = open('files/sample.txt').read().lower().split()
+bookWords = open('files/sample.txt').read().decode("unicode-escape").encode("ascii", "ignore").lower().split()
 bookWords = [el.rstrip(string.punctuation).lstrip(string.punctuation) for el in bookWords]
 
 #print bookWords
@@ -26,8 +26,16 @@ from collections import Counter
 topWords = Counter(finalWords)
 #print topWords
 
-frequent = 20
+total = 0
+
+for w in topWords:
+    total += topWords[w]
+
+frequent = total/(len(topWords))
 
 tops = {k:v for (k,v) in topWords.iteritems() if v > frequent}
 
-print tops
+final = sorted(tops.items(), key=lambda x: x[1], reverse=True)
+
+for x in range(len(final)):
+    print '%s: %s' % (final[x][0], final[x][1])
